7507d7cbba2ea040faa724791330b9dc97cd6676
Robert George <rgeorge1982@gmail.com>
2021-07-22 18:13:25 -0400

    fixed a weird bug

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -22,7 +22,6 @@ const useStyles = makeStyles(() => ({
 const Sidebar = (props) => {
   const classes = useStyles();
   const conversations = props.conversations || [];
-  console.log(conversations)
   const { handleChange, searchTerm } = props;
 
   return (
